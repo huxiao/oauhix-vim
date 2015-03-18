8e7a299830baf8db91498d382272ab6dc6c20501
huxiao <huxiao1979@gmail.com>
2015-03-18 14:50:23 +0800

    Initial commit
    在github上使用spf13-vim-3的配置，之前的情况是：先fork，后开分支，提交自己的.fork文件，
    然后不断的merge跟进。现改变用法为：将spf13-vim-3作为oauhix-vim的子模块跟进更新，使目
    录结构更清晰。
    
    oauhix-vim作为vi/vi-like软件的配置集合，支持的软件目前包括：
        * vim/gvim
        * vifm
        * zathura
        * vimperator

---
+++ README.markdown
@@ -0,0 +1,30 @@
+# oauhix-vim : Fork form Steve Francia's Vim Distribution
+
+                    __ _ _____              _
+         ___ _ __  / _/ |___ /      __   __(_)_ __ ___
+        / __| '_ \| |_| | |_ \ _____\ \ / /| | '_ ` _ \
+        \__ \ |_) |  _| |___) |_____|\ V / | | | | | | |
+        |___/ .__/|_| |_|____/        \_/  |_|_| |_| |_|
+            |_|
+
+spf13-vim is a distribution of vim plugins and resources for Vim, Gvim and [MacVim].
+
+It is a good starting point for anyone intending to use VIM for development running equally well on Windows, Linux, \*nix and Mac.
+
+The distribution is completely customisable using a `~/.vimrc.local`, `~/.vimrc.bundles.local`, and `~/.vimrc.before.local` Vim RC files.
+
+![spf13-vim image][spf13-vim-img]
+
+感谢spf13-vim项目及所有贡献者，让配置vim变得如此轻松！
+
+oauhix-vim是我的vi/vi-like软件的配置集合，以spf13-vim-3为子模块，支持的软件目前包括：
+    * vim/gvim
+    * vifm
+    * zathura
+    * vimperator
+
+# Installation
+
+
+
+[spf13-vim-img]:https://i.imgur.com/UKToY.png
---
+++ rclink.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+oauhiinst="$HOME/.oauhix-vim/oauhix-vim"
+spf13inst="$HOME/.spf13-vim-3"
+
+lnif() {
+    if [ -e "$1" ]; then
+        ln -sf "$1" "$2"
+    fi
+    ret="$?"
+}
+
+lnif "$spf13inst/.vimrc"                    "$HOME/.vimrc"
+lnif "$spf13inst/.vimrc.before"             "$HOME/.vimrc.before"
+lnif "$spf13inst/.vimrc.bundles"            "$HOME/.vimrc.bundles"
+lnif "$spf13inst/.vim"                      "$HOME/.vim"
+
+lnif "$oauhiinst/.vimrc.fork"              "$HOME/.vimrc.fork"
+lnif "$oauhiinst/.vimrc.before.fork"       "$HOME/.vimrc.before.fork"
+lnif "$oauhiinst/.vimrc.bundles.fork"      "$HOME/.vimrc.bundles.fork"
+lnif "$oauhiinst/.gvimrc"                  "$HOME/.gvimrc"
+
+rm -rf "$HOME/.config/.vifm"
+mkdir -p "~/.config/zathura"
+
+lnif "$oauhiinst/others/vifm"               "$HOME/.vifm"
+lnif "$oauhiinst/others/zathura"            "$HOME/.config/zathura"
+lnif "$oauhiinst/others/vifm"               "$HOME/.vifm"
+
+# Useful for fork maintainers
+touch  "$HOME/.vimrc.local"
+touch  "$HOME/.gvimrc.local"
+touch  "$HOME/.vimrc.before.local"
+touch  "$HOME/.vimrc.bundles.local"
+
+if [[ ! -e "$spf13inst" ]]; then
+    ln -s "$HOME/.oauhix-vim/spf13-vim" "$spf13inst"
+fi
