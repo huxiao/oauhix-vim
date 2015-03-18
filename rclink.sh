3ae32f9496b7fbe092c63ffcc80da8f1cafc7cb5
huxiao <huxiao1979@gmail.com>
2015-03-18 16:25:22 +0800

    添加非vim软件的配置文件
    在oauhix-vim/otherconf中放置一些诸如zsh/vimperator的配置文件。

--- rclink.sh
+++ rclink.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-oauhiinst="$HOME/.oauhix-vim/oauhix-vim"
+ovroot="$HOME/.oauhix-vim"
+vimcustom="$ovroot/oauhix-vim"
 spf13inst="$HOME/.spf13-vim-3"
 
 lnif() {
@@ -15,16 +16,19 @@ lnif "$spf13inst/.vimrc.before"             "$HOME/.vimrc.before"
 lnif "$spf13inst/.vimrc.bundles"            "$HOME/.vimrc.bundles"
 lnif "$spf13inst/.vim"                      "$HOME/.vim"
 
-lnif "$oauhiinst/.vimrc.fork"               "$HOME/.vimrc.fork"
-lnif "$oauhiinst/.vimrc.before.fork"        "$HOME/.vimrc.before.fork"
-lnif "$oauhiinst/.vimrc.bundles.fork"       "$HOME/.vimrc.bundles.fork"
-lnif "$oauhiinst/.gvimrc"                   "$HOME/.gvimrc"
+lnif "$vimcustom/.vimrc.fork"               "$HOME/.vimrc.fork"
+lnif "$vimcustom/.vimrc.before.fork"        "$HOME/.vimrc.before.fork"
+lnif "$vimcustom/.vimrc.bundles.fork"       "$HOME/.vimrc.bundles.fork"
+lnif "$vimcustom/.gvimrc"                   "$HOME/.gvimrc"
 
 rm -rf "$HOME/.config/.vifm"
 mkdir -p "$HOME/.config/zathura"
 
-lnif "$oauhiinst/others/vifm"               "$HOME/.vifm"
-lnif "$oauhiinst/others/zathurarc"          "$HOME/.config/zathura/zathurarc"
+lnif "$vimcustom/others/vifm"               "$HOME/.vifm"
+lnif "$vimcustom/others/zathurarc"          "$HOME/.config/zathura/zathurarc"
+
+lnif "$ovroot/otherconf/.vimperatorrc"      "$HOME/.vimperatorrc"
+lnif "$ovroot/otherconf/.zshrc"             "$HOME/.zshrc"
 
 # Useful for fork maintainers
 touch  "$HOME/.vimrc.local"
